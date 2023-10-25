dc212fc0b396806d03814c546521aaf794a30dc4
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-10-25 14:49:47 -0300

    Not found route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import ClientHome from './routes/ClientHome';
 import Catalog from './routes/ClientHome/Catalog';
@@ -14,6 +14,7 @@ function App() {
           <Route index element={<Catalog />} />
           <Route path='catalog' element={<Catalog />} />
           <Route path='product-details/:productId' element={<ProductDetails />} />
+          <Route path='*' element={<Navigate to={"/"} />} />
         </Route>
       </Routes>
     </BrowserRouter>
