9d75b6a17ceab84400b6ca2bfec1f4b4f76ac0cb
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-11-15 13:34:48 -0300

    Cart mock data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import ClientHome from './routes/ClientHome';
 import Catalog from './routes/ClientHome/Catalog';
+import Cart from './routes/ClientHome/Cart';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Catalog />} />
           <Route path='catalog' element={<Catalog />} />
           <Route path='product-details/:productId' element={<ProductDetails />} />
+          <Route path='cart' element={<Cart />} />
         </Route>
         <Route path='*' element={<Navigate to={"/"} />} />
       </Routes>
