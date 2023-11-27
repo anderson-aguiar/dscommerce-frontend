90b912f47ccb77cb24e29f3f7674c728f892846d
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-11-27 15:54:30 -0300

    Searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import './styles.css';
-export default function SearchaBar() {
+
+type Props = {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onSearch: Function;
+}
+
+export default function SearchaBar({ onSearch } : Props) {
+
+    const [text, setText] = useState("");
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
+        setText(event.target.value);
+    }
+
+    function handleSubmit(event: React.FormEvent){
+        event.preventDefault();
+        onSearch(text);
+    }
+    function handleResetClick(){
+        setText("");
+        onSearch(text);
+    }
+
     return (
-        <form className="dsc-search-bar">
+        <form className="dsc-search-bar" onSubmit={handleSubmit}>
             <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produtos" />
-            <button type="reset">🗙</button>
+            <input value={text} type="text" placeholder="Nome do produtos" onChange={handleChange} />
+            <button onClick={handleResetClick}>🗙</button>
         </form>
     );
 }
