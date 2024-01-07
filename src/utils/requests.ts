0cef02352fe18798d86ca8e7771bcb4ffdcc84d3
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-01-07 16:45:34 -0300

    requestBackend update withCredentials

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
+import * as authService from '../services/auth-service';
 
 export function requestBackend(config : AxiosRequestConfig){
-    return axios({...config, baseURL: BASE_URL});
+    //testa se as configurações está com credenciais para acesso autorizado, se sim, acrescenta o "Authorization: .."
+    const headers = config.withCredentials 
+    ? {
+        ...config.headers,
+        Authorization: "Bearer " + authService.getAccessToken() 
+    }
+    : config.headers
+    return axios({...config, baseURL: BASE_URL, headers});
+
 }
