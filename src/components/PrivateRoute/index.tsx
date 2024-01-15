0a1e95d2e75cdc4aba3a9b6d6a7913211d8e3988
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-01-15 14:30:32 -0300

    Access control route level

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,13 +1,18 @@
 import { Navigate } from "react-router-dom";
 import * as authService from '../../services/auth-service';
+import { RoleEnum } from "../../models/auth";
 
 type Props = {
     children: JSX.Element;
+    roles?: RoleEnum[];
 }
 //Verifica se o usuario está logado, se não estiver, direciona para o "/login"
-export function PrivateRoute({children} : Props){
+export function PrivateRoute({children, roles = []} : Props){
     if(!authService.isAuthenticated()){
         return <Navigate to="/login" />
+    }
+    if (!authService.hasAnyRoles(roles)) {
+        return <Navigate to='/catalog' />
     }
     return children;
 }
