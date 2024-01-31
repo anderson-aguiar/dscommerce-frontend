ddfba32debf835ce8c7614601f3a094d22cd9b0e
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-01-31 14:09:22 -0300

    forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,7 +1,17 @@
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 export function update(inputs: any, name: string, newValue: any){
 
     return { ...inputs, [name] : {...inputs[name], value: newValue} };
+}
+
+export function toValues(inputs: any){
+    const data: any = {};
+
+    for( var name in inputs){
+        data[name] = inputs[name].value;
+    }
+    return data;
 }
