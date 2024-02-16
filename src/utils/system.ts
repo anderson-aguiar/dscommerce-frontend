2e83835098e321aa419cd881989dfe72fb25fa14
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-02-16 14:33:52 -0300

    Update system.ts

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,6 @@
 export const CART_KEY = "anderson.dscommerce/Cart";
 export const TOKEN_KEY = "anderson.dscommerce/Token";
 
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BASE_URL ?? "http://localhost:8080";
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ??  "myclientid";
 export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
